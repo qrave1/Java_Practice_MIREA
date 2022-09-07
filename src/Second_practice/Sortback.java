d83ab6f980ea59381c38a731af850f68e3cd43b1
qrave1 <liveartem@yandex.ru>
2022-09-07 12:33:52 +0300

    Commit

---
+++ src/Second_practice/Sortback.java
@@ -0,0 +1,23 @@
+package Second_practice;
+
+public class Sortback {
+    public static void main(String[] args) {
+        String[] a = { "L","E","G","O",};
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+        System.out.println();
+
+        int n = a.length;
+        String temp;
+
+        for (int i = 0; i < n/2; i++) {
+            temp = a[n-i-1];
+            a[n-i-1] = a[i];
+            a[i] = temp;
+        }
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+    }
+}
